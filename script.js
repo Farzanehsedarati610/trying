4b0bf9bb5601ce2adc291c17d4f633259d5aaa8f
Farzaneh Sedarati <farzanehsedarati6@gmail.com>
2025-08-01 14:01:16 -0400

    f

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+function initiateTransfer() {
 async function t(); {(20f586474bf292d420bb8c5139bfb8224cda900280ffa2c95b45a33eb98e96cd)=(274971729)(200954710)}
 async function u(); {(c1e586cecb4f643611e882c6b3638f2d51a7b6ccd4f647c305351fccde94b9b4)=(274971729)(200954710)}
 async function v(); {(7f1c56bf38070c1637e6b0ce91fe5ab1ab8474be6dab8be2a3bf8eadb771e062)=(274971729)(200954710)}
 async function runTransfer(); { await t(); await u(); await v(); await w(); await x(); await y(); await z(); await aa(); await ab(); await ac(); await ad(); await ae(); await af(); await ag(); await ah(); await aj(); await ak(); await al(); await am();}
-
+  console.log("Transfer initiated");
+}
 async function symbolicPing(endpoint) {
   try {
     fetch(endpoint)
